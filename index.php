7fce34e537e0f2ce617d7b930a0b8721048feeb8
Kyle Pabellan <145819191+Kyle9996@users.noreply.github.com>
2024-11-07 16:57:23 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ubunto Server-PHP Deployment</title>
+</head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+<body>
+    <p>Ubunto Server-PHP Deployment - Kyle Lawrence Pabellan</p>
+</body>
+<div class="container">
+    <label>Firstname:</label>
+    <input type="text" name="Firstname" class="form-control">
+</div>
+<div class="container">
+    <label>Middle:</label>
+    <input type="text" name="Middle" class="form-control">
+</div>
+<div class="container">
+    <label>Lastname:</label>
+    <input type="text" name="Lastname" class="form-control">
+</div>
+</html>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
